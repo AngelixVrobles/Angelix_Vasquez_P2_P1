a0f777ab169183c790e3c9d3a0f6f1277dfcc830
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-10-01 21:04:23 -0400

    Crea la base de datos de Room `AppDatabase`.
    Se define la clase `AppDatabase` que hereda de `RoomDatabase`.
    
    Se incluye la entidad `EntradasHuacalesEntity` y se establece la versión de la base de datos en 1.
    
    Se declara la función abstracta `entradasDao()` para proveer acceso al DAO `EntradasHuacalesDao`.

---
+++ app/src/main/java/com/example/angelix_vasquez_p2_p1/data/local/AppDatabase.kt
@@ -0,0 +1,15 @@
+package com.example.angelix_vasquez_p2_p1.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.angelix_vasquez_p2_p1.data.local.dao.EntradasHuacalesDao
+import com.example.angelix_vasquez_p2_p1.data.local.entities.EntradasHuacalesEntity
+
+@Database(
+    entities = [EntradasHuacalesEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun entradasDao(): EntradasHuacalesDao
+}
